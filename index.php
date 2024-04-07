9824138dd17aeb366e1b5f2b9da1055e53d7a908
Sanchez <selutin.alexandr90@gmail.com>
2024-04-07 19:18:45 +0300

    add style

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>To-Do list</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>To-Do List</h1>
+        <form action="/add.php" method="post">
+            <input type="text" name="task" id="task" placeholder="To Do" class="form-control">
+            <button type="submit" name="sendTask" class="btn btn-success">Send</button>
+        </form>
+    </div>
 </body>
 </html>
